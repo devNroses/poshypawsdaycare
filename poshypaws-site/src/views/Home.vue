e85a8cf51b23303f479af796705dfd2b11b2313c
Jon Selby <jselbygfx1@gmail.com>
2019-01-28 23:15:34 -0700

    Refactor: Cleaned out node package errors. Updated Cli features.

---
+++ poshypaws-site/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <app-landing></app-landing>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Landing from "@/modules/home/index.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    appLanding: Landing
+  }
+})
+export default class Home extends Vue {}
+</script>
