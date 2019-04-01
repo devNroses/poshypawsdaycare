0e366b4d98f6d56881fc66cb14465443037f42b4
Jon Selby <jselbygfx1@gmail.com>
2019-04-01 00:11:47 -0700

    feature(site) Added two new sections to landingpage index and updated structure for sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,41 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">GitHub</a>
-      </card>
+  <div>
+    <section>
+      <div class="section--wrapper">
+        <div class="section--mainContent">
+          <img src="../assets/img/dogGrooming.jpg" alt="Dog Grooming">
+        </div>
+        <div class="section--content">
+          <h1>DOG GROOMING</h1>
+          <h3>We Cater to all your dogs needs</h3>
+          <p>
+            We are a family owned and operated business. We take tremendous
+            care to be gentle and kind with your pet while getting them completely
+            groomed to the highest standards.
+          </p>
+        </div>
+      </div>
+    </section>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">Every</b> component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">Vue.js</a> and
-        <a href="http://bulma.io/">Bulma</a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">No other internal dependency</card>
-    </div>
-  </section>
+    <section>
+      <div class="section--wrapper">
+        <div class="section--content_fullwidth">
+          <div class="section--mainContent section--content" style="margin-left: 0px;">
+            <h1>OUR SERVICES</h1>
+            <h3>Day with Poshy Paws</h3>
+            <p>
+              We want your furry family member to be so excited to be here. With a full array of services, which
+              includes dog wash packages or items such as nail trimming/painting and dog daycare. We strive
+              to provide a place to come in and instantly relax for not only you but your dog as well.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import Component from 'vue-class-component'
-
-Component
 export default class HomePage extends Vue {}
 </script>
