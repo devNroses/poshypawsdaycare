4a15047bea7db8e58be6fa8eb1b4d8f1a718efe7
Jon Selby <jselbygfx1@gmail.com>
2019-06-06 23:39:37 -0700

    (Poshy Paws) Landing: Added footer with the contact information. Next need to set up the about us/ contact page template

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,5 +46,74 @@
     <div class="container column is-12">
       <nuxt/>
     </div>
+
+    <div class="footer">
+      <div></div>
+      <div class="footer--content">
+        <h3>Great spa day for your dog</h3>
+        <p>
+          If you like to schedule grooming,
+          boarding, or have any other questions,
+          we’re here to help.
+        </p>
+        <button class="button--contact">Contact</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.footer {
+  width: 100%;
+  height: 600px;
+  color: white;
+  margin: 200px auto 50px;
+  display: flex;
+  justify-content: flex-start;
+  padding: 0;
+
+  div {
+    background: #4ec4d1;
+    width: 50%;
+    height: auto;
+    flex-grow: 1;
+    flex-wrap: wrap;
+
+    &:first-child {
+      width: 45%;
+      background-image: url('../assets/img/footer_dog.jpg');
+      background-position: left center;
+    }
+  }
+
+  &--content {
+    padding: 50px;
+    h3 {
+      margin-top: 18%;
+      font-size: 20px;
+    }
+
+    p {
+      font-size: 32px;
+      font-weight: bold;
+      margin: 20px auto;
+    }
+  }
+}
+.button {
+  &--contact {
+    width: 150px;
+    height: auto;
+    padding: 10px;
+    font-size: 18px;
+    background: transparent;
+    color: white;
+    border: 2px solid white;
+
+    &:hover {
+      background: white;
+      color: #4ec4d1;
+    }
+  }
+}
+</style>
